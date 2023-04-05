1262245e812406a7d9b93a47cd59786a653d1f6e
Iryna Temchuk <itemchuk98@gmail.com>
2023-04-05 17:48:46 +0300

    Fix script

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let more = document.querySelectorAll(".readmore-btn");
 
 for (let i = 0; i < more.length; i++) {
     more[i].addEventListener("click", function() {
-        more[i].parentNode.classList.toggle("active")
-    })
+        more[i].parentNode.classList.toggle("active");
+    });
 }
